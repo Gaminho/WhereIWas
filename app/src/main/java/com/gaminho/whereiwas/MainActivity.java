ba42aace4bf376562362636db130792369639dcc
ariche <riche.ant@gmail.com>
2018-07-21 14:48:29 +0200

    add Firebase configuration for Database and Storage.

--- app/src/main/java/com/gaminho/whereiwas/MainActivity.java
+++ app/src/main/java/com/gaminho/whereiwas/MainActivity.java
@@ -2,12 +2,27 @@ package com.gaminho.whereiwas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class MainActivity extends AppCompatActivity {
+
+    private StorageReference mStorageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("test");
+        myRef.setValue("Second saved message");
     }
 }
